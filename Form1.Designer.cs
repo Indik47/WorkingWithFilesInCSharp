3cbf920ecacf4b028ebb2250040750aca14e966c
Indik47 <indik47@gmail.com>
2018-03-20 17:50:37 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WorkingWithFilesInCSh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCurrentDirectory = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxFileStream = new System.Windows.Forms.TextBox();
+            this.buttonFileStreamWrite = new System.Windows.Forms.Button();
+            this.buttonFileStreamRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCurrentDirectory
+            // 
+            this.buttonCurrentDirectory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCurrentDirectory.Location = new System.Drawing.Point(136, 122);
+            this.buttonCurrentDirectory.Name = "buttonCurrentDirectory";
+            this.buttonCurrentDirectory.Size = new System.Drawing.Size(151, 44);
+            this.buttonCurrentDirectory.TabIndex = 0;
+            this.buttonCurrentDirectory.Text = "DirectoryAndFileInfo";
+            this.buttonCurrentDirectory.UseVisualStyleBackColor = true;
+            this.buttonCurrentDirectory.Click += new System.EventHandler(this.buttonCurrentDirectory_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(136, 172);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(393, 225);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // textBoxFileStream
+            // 
+            this.textBoxFileStream.Location = new System.Drawing.Point(624, 172);
+            this.textBoxFileStream.Multiline = true;
+            this.textBoxFileStream.Name = "textBoxFileStream";
+            this.textBoxFileStream.Size = new System.Drawing.Size(500, 225);
+            this.textBoxFileStream.TabIndex = 2;
+            // 
+            // buttonFileStreamWrite
+            // 
+            this.buttonFileStreamWrite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFileStreamWrite.Location = new System.Drawing.Point(624, 122);
+            this.buttonFileStreamWrite.Name = "buttonFileStreamWrite";
+            this.buttonFileStreamWrite.Size = new System.Drawing.Size(151, 44);
+            this.buttonFileStreamWrite.TabIndex = 0;
+            this.buttonFileStreamWrite.Text = "FileStreamWrite";
+            this.buttonFileStreamWrite.UseVisualStyleBackColor = true;
+            this.buttonFileStreamWrite.Click += new System.EventHandler(this.buttonFileStreamWrite_Click);
+            // 
+            // buttonFileStreamRead
+            // 
+            this.buttonFileStreamRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFileStreamRead.Location = new System.Drawing.Point(799, 122);
+            this.buttonFileStreamRead.Name = "buttonFileStreamRead";
+            this.buttonFileStreamRead.Size = new System.Drawing.Size(151, 44);
+            this.buttonFileStreamRead.TabIndex = 0;
+            this.buttonFileStreamRead.Text = "FileStreamRead";
+            this.buttonFileStreamRead.UseVisualStyleBackColor = true;
+            this.buttonFileStreamRead.Click += new System.EventHandler(this.buttonFileStreamRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 438);
+            this.Controls.Add(this.textBoxFileStream);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonFileStreamRead);
+            this.Controls.Add(this.buttonFileStreamWrite);
+            this.Controls.Add(this.buttonCurrentDirectory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCurrentDirectory;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxFileStream;
+        private System.Windows.Forms.Button buttonFileStreamWrite;
+        private System.Windows.Forms.Button buttonFileStreamRead;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace WorkingWithFilesInCSh
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "C:\\Users\\d.oligov\\Csharp\\dftg.txt";
+        FileStream fs;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCurrentDirectory_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo currDir = new DirectoryInfo(@"C:\Users\d.oligov");
+            textBoxOutput.Text = "FullName = " + currDir.FullName;
+            textBoxOutput.AppendText(Environment.NewLine + "Name = " + currDir.Name);
+            textBoxOutput.AppendText(Environment.NewLine + "Parent = " + currDir.Parent.ToString());
+            textBoxOutput.AppendText(Environment.NewLine + "Attributes = " + currDir.Attributes);
+            textBoxOutput.AppendText(Environment.NewLine + "Creation time = " + currDir.CreationTime);
+
+            //string directoryPath = String.Format(textBoxDirectoryPath.Text);  //get directory path from textBox
+
+            DirectoryInfo newDir = new DirectoryInfo(@"C:\Users\d.oligov\Csharp");
+            if (!newDir.Exists) { newDir.Create(); }                        //creare directory if it doesnt exist
+            
+            string filePath = newDir.FullName + "\\a.txt";
+            
+
+            string textToWrite = textBoxOutput.Text;                   //get text from textBox
+            
+            File.WriteAllText(filePath, textToWrite);                  //write it to file
+
+            //output all lines that were read from file into textBox
+            int lineCounter = 0;
+            foreach (string name in File.ReadAllLines(filePath))
+            {
+                lineCounter++;
+                textBoxOutput.AppendText(String.Format(Environment.NewLine + "{0,-20} : {1}", lineCounter, name));
+            }
+
+            //get txt files fro, directory and its subdirs
+            DirectoryInfo myDataDir = new DirectoryInfo(@"C:\Users\d.oligov\Csharp");
+            FileInfo[] txtFiles = myDataDir.GetFiles("*.txt",SearchOption.AllDirectories);
+
+            //print them out to textBoxileInfo fi in txtFiles)
+            foreach (FileInfo fi in txtFiles)
+            {
+                textBoxOutput.AppendText(Environment.NewLine + fi.FullName + " size: " + fi.Length);
+            }
+            
+        }
+
+        private void buttonFileStreamWrite_Click(object sender, EventArgs e)
+        {
+            fs = File.Open(filePath, FileMode.OpenOrCreate);                            //create FileStream
+            string randomString = "cucu ruku" + Environment.NewLine + "master juku";    //make some string(random text)
+            byte[] bytesFromRandomString = Encoding.Default.GetBytes(randomString);     //convert it to bytes and write into byte array
+
+            fs.Write(bytesFromRandomString, 0, bytesFromRandomString.Length);           //write FileStream to file
+
+            fs.Close();
+        }
+
+        private void buttonFileStreamRead_Click(object sender, EventArgs e)
+        {
+            fs = File.Open(filePath, FileMode.OpenOrCreate);    //open FileStream
+            FileInfo file = new FileInfo(filePath);             //make FileInfo var for retrieving file length later
+            
+            byte[] byteArrFromFile = new byte[file.Length];     //create byte array same length as file
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                byteArrFromFile[i] = (byte)fs.ReadByte();       //use FileStream to read file byte by byte
+            }
+            textBoxFileStream.AppendText(Encoding.Default.GetString(byteArrFromFile));  //convert FileStream to string and output to textBox
+
+            fs.Close();
+
+        }
+    }
+}
